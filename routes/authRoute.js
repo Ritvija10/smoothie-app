9a43ed83402918b401424e14a494027e0c787842
Ritvija10 <ritvijain05@gmail.com>
2026-01-16 16:14:03 +0530

    Add files via upload

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,6 +5,7 @@ console.log("signup_get:", authController.signup_get);
 console.log("signup_post:", authController.signup_post);
 console.log("login_get:", authController.login_get);
 console.log("login_post:", authController.login_post);
+
 router.get('/signup',authController.signup_get);
 router.post('/signup',authController.signup_post);
 
